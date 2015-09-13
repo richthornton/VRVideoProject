00d7c51e6945f80b02bdc3f05cd16900b801f481
Richard Thornton <rithornt@cisco.com>
2015-09-13 19:09:29 +0100

    VR Video Project
    First Commit for the VR Video project

---
+++ Assets/Scripts/webcamScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class webcamScript : MonoBehaviour {
+	
+	//The texture that holds the video captured by the webcam
+	private WebCamTexture webCamTexture;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		//Initialize the webCamTexture
+		webCamTexture = new WebCamTexture();
+		//Assign the images captured by the first available webcam as the texture of the containing game object
+		GetComponent<Renderer>().material.mainTexture = webCamTexture;
+		//Start streaming the images captured by the webcam into the texture
+		webCamTexture.Play();
+	}
+}
